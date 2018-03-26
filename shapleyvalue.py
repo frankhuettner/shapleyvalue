9e1502f02b74f1f0609710f2a98aa54b2fdb0ba4
frankhuettner <36417105+frankhuettner@users.noreply.github.com>
2018-03-26 23:16:31 +0200

    added minimalcoalitionsize

--- shapleyvalue.py
+++ shapleyvalue.py
@@ -2,7 +2,7 @@ import itertools
 from pprint import pprint as pprint
 from timeit import default_timer as timer
 
-def shapleyvalue(playerset,v):
+def shapleyvalue(playerset,v,minimalWinningCoalitionSize=1):
     ''' input,  set, the player set
                 function, TU game which assigns to every coalition a worth i.e. a set function f:2^n -> R
         output, dict, stores the Shapley values
@@ -11,11 +11,11 @@ def shapleyvalue(playerset,v):
     SV = {}    # dict storing the Shapley values
     potCur = {}    # stores potential values of coalition with size k
 
-    for player in playerset:
-        player_as_singleton_set = frozenset([player])
-        potCur[player_as_singleton_set]=v(player_as_singleton_set) # set pot of singletons to worth of singletons
+    for coalition in itertools.combinations(playerset, minimalWinningCoalitionSize):
+        potCur[frozenset(coalition)]=v(frozenset(coalition))/(minimalWinningCoalitionSize) # pot(S \cup i) = v(S \cup i)/ (s+1)   + 0 if pot(S \setminus i) = 0 for all i \in S 
 
-    for coalition_Size in range(1,n-1):
+
+    for coalition_Size in range(minimalWinningCoalitionSize,n-1):
         potNext = {}    # stores next potential values of coalition with size coalition_Size+1
         for coalition in itertools.combinations(playerset, coalition_Size+1):
             potNext[frozenset(coalition)]=v(frozenset(coalition))/(coalition_Size+1) # pot(S \cup i) = v(S \cup i)/ (s+1)   +...
@@ -61,19 +61,6 @@ def UN_security_council(coalition):
     elif not coalition.issuperset({ 'CH', 'FR','RUS','UK', 'US'}): return 0
     else: return 1    
 
-EU27POP = [289714776,8772865,11351727,7101859,4154213,854802,10578820,5748769,1315635,5503297,66989083,82521653,10768193,9797561,4784383,60589445,1950116,2847904,590667,460297,17081507,37972964,10309573,19644350,5435343,2065895,46528966,9995153]
-def EU27(coalition):
-    ''' input, a set containing players out of the player set {1,...,27}
-        output, 1 if 
-                coalition size > 24 
-                or
-                coalition size > 15 and sum of player's population surpasses 0.65* 445715040 = 289714776
-    '''
-    if len(coalition)<16: return 0
-    elif len(coalition)>24: return 1
-    elif  sum([EU27POP[i] for i in coalition]  ) <289714776: return 0 #here we know that between 16 and 24 countries support and check for population threshold
-    else: return 1
-##################################################################################################################
 
 
 start = timer()
@@ -81,12 +68,9 @@ start = timer()
 result = shapleyvalue({1,2,3},v)
 
 ###### uncomment this for calculation of Shapley Shubik of the UN Security Council (takes a second)
-# result = shapleyvalue(frozenset(['US', 'CH', 'RUS', 'FR','UK']).union(frozenset(range(1,11))), UN_security_council ) 
+result = shapleyvalue(frozenset(['US', 'CH', 'RUS', 'FR','UK']).union(frozenset(range(1,11))), UN_security_council, minimalWinningCoalitionSize=9) 
 ######
 
-###### uncomment this for calculation of Shapley Shubik of the EU Council without England  (takes about an hour)
-# result = shapleyvalue( frozenset(range(27)), EU27)  
-######
 
 end = timer()
 ctime = end - start
